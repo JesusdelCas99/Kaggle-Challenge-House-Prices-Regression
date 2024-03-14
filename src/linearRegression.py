a8fc0148fb54615eadab985c2730f9464462325a
JesusdelCas99 <grjesus99@gmail.com>
2024-03-14 21:54:37 +0100

    Update linear regression submission file

--- src/linearRegression.py
+++ src/linearRegression.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def one_hot_encode_dataset(data: pd.DataFrame):
-    # Identificar variables categóricas
+    # Identify categorical variables
     categorical_features = data.select_dtypes(include=['object']).columns
 
-    # Aplicar one-hot encoding a las variables categóricas
+    # Apply one-hot encoding to the categorical variables
     encoded_data = pd.get_dummies(data, columns=categorical_features)
 
     return encoded_data
